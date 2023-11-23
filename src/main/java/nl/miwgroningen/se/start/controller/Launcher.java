e605a44229fc1bade3e212299c6c687be78444f5
Seyma <knt.seyma@gmail.com>
2023-11-23 16:38:21 +0100

    Adding WordMap class

--- src/main/java/nl/miwgroningen/se/start/controller/Launcher.java
+++ src/main/java/nl/miwgroningen/se/start/controller/Launcher.java
@@ -1,6 +1,10 @@
 package nl.miwgroningen.se.start.controller;
 
+import nl.miwgroningen.se.start.model.WordMap;
 import nl.miwgroningen.se.start.model.WordSet;
+
+import java.sql.SQLOutput;
+import java.util.Iterator;
 
 /**
  * @author Vincent Velthuizen <v.r.velthuizen@pl.hanze.nl>
@@ -10,14 +14,35 @@ import nl.miwgroningen.se.start.model.WordSet;
 public class Launcher {
 
     public static void main(String[] args) {
-        WordSet wordSet = new WordSet();
+//        WordSet wordSet = new WordSet();
 
-        wordSet.readFromFile("src/main/resources/MaxHavelaar.txt");
+        WordMap wordMap = new WordMap();
 
-        System.out.println(wordSet.getNrOfUniqueWords());
+        wordMap.readFromFile("src/main/resources/MaxHavelaar.txt");
 
-        for (String word : wordSet.getWordsSorted()) {
+        System.out.println("Number of unique words: " + wordMap.getNrOfUniqueWords());
+
+        System.out.println(wordMap.getNrOfUniqueWords());
+        for (String word : wordMap.getWordsSorted()) {
             System.out.println(word);
+        }
+
+        // Print de woorden in alfabetische volgorde met hun frequentie
+        System.out.println("Woorden in alfabetische volgorde met frequentie:");
+        Iterator<String> iterator = wordMap.getWordsSorted().iterator();
+        while (iterator.hasNext()) {
+            String word = iterator.next();
+            int count = wordMap.getWordCount(word);
+            System.out.println(word + ": " + count + " keer");
         }
     }
 }
+
+//        wordSet.readFromFile("src/main/resources/MaxHavelaar.txt");
+//
+//        System.out.println(wordSet.getNrOfUniqueWords());
+//
+//        for (String word : wordSet.getWordsSorted()) {
+//            System.out.println(word);
+//        }
+//
---
+++ src/main/java/nl/miwgroningen/se/start/model/WordMap.java
@@ -0,0 +1,53 @@
+package nl.miwgroningen.se.start.model;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+/**
+ * @author Seyma Kanat <s.kanat@st.hanze.nl>
+ * WordMap class
+ */
+public class WordMap {
+    private Map<String, Integer> wordMap = new HashMap<>();
+
+    public void readFromFile(String filename) {
+        File sourceFile = new File(filename);
+
+        try (Scanner sourceReader = new Scanner(sourceFile)) {
+            while (sourceReader.hasNext()) {
+                String word = sourceReader.next();
+
+                word = word.replaceAll("[^A-Za-z0-9]", "");
+                if (!word.equals("")) {
+                    wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+                }
+            }
+        } catch (FileNotFoundException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        }
+    }
+
+    public int getNrOfUniqueWords() {
+        return wordMap.size();
+    }
+
+    public Iterable<String> getWordsSorted() {
+        //1.Make a list
+        List<String> words = new ArrayList<>();
+
+        // make hashset for iterator
+//        wordMap.keySet();
+        for (String word : wordMap.keySet()) {
+            words.add(word);
+        }
+        //crate String from hashmap save it to the list
+        Collections.sort(words);
+
+        return words;
+    }
+
+    public int getWordCount(String word) {
+        return wordMap.getOrDefault(word, 0);
+    }
+}
